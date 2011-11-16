5bf393ff22595b5d67429df4b2734cac2c3b908c
Michael Uzquiano <mgu1@cornell.edu>
2011-11-16 12:11:06 -0600

    Support for attachments on organizations + test

--- src/main/webapp/js_src/gitana/objects/Organization.js
+++ src/main/webapp/js_src/gitana/objects/Organization.js
@@ -371,6 +371,115 @@
         getAssignedRepositoryIds: function()
         {
             return this.get("repositories");
+        },
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////
+        //
+        // ATTACHMENTS
+        //
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+        /**
+         * Hands back an attachments map.
+         *
+         * @chained attachment map
+         *
+         * @public
+         */
+        listAttachments: function()
+        {
+            var self = this;
+
+            var attachmentMap = new Gitana.BinaryAttachmentMap(this);
+
+            var result = this.subchain(attachmentMap);
+            result.subchain().then(function() {
+
+                var chain = this;
+
+                self.getDriver().gitanaGet(self.getUri() + "/attachments", null, function(response) {
+
+                    var map = {};
+                    for (var i = 0; i < response.rows.length; i++)
+                    {
+                        map[response.rows[i]["_doc"]] = response.rows[i];
+                    }
+                    attachmentMap.handleMap(map);
+
+                    chain.next();
+                });
+
+                return false;
+            });
+
+            return result;
+        },
+
+        /**
+         * Picks off a single attachment
+         *
+         * @chained attachment
+         *
+         * @param attachmentId
+         */
+        attachment: function(attachmentId)
+        {
+            return this.listAttachments().select(attachmentId);
+        },
+
+        /**
+         * Creates an attachment.
+         *
+         * When using this method from within the JS driver, it really only works for text-based content such
+         * as JSON or text.
+         *
+         * @chained attachment
+         *
+         * @param attachmentId (use null or false for default attachment)
+         * @param contentType
+         * @param data
+         */
+        attach: function(attachmentId, contentType, data)
+        {
+            var self = this;
+
+            // the thing we're handing back
+            var result = this.subchain(new Gitana.BinaryAttachment(this, attachmentId));
+
+            // preload some work onto a subchain
+            result.subchain().then(function() {
+
+                // upload the attachment
+                var uploadUri = self.getUri() + "/attachments/" + attachmentId;
+                this.chainUpload(this, uploadUri, null, contentType, data).then(function() {
+
+                    // read back attachment information and plug onto result
+                    this.subchain(self).listAttachments().select(attachmentId).then(function() {
+                        result.handleAttachment(this.attachment);
+                    });
+                });
+            });
+
+            return result;
+        },
+
+        /**
+         * Deletes an attachment.
+         *
+         * @param attachmentId
+         */
+        unattach: function(attachmentId)
+        {
+            return this.subchain().then(function() {
+
+                this.chainDelete(this, this.getUri() + "/attachments/" + attachmentId).then(function() {
+
+                    // TODO
+
+                });
+            });
         }
 
     });
---
+++ src/main/webapp/tests/js_src/testOrganizationAttachments1.js
@@ -0,0 +1,93 @@
+(function($) {
+
+    module("organizationAttachments1");
+
+    // Test case : Organization Attachments
+    test("Organization Attachments 1", function()
+    {
+        stop();
+
+        expect(13);
+
+        var test = this;
+
+        var gitana = new Gitana();
+        gitana.authenticate("admin", "admin").then(function() {
+
+            // NOTE: this = server
+
+            // create an organization
+            this.createOrganization().then(function() {
+
+                // NOTE: this = organization
+
+                // create a bunch of text attachments
+                this.attach("attachmentId1", "text/plain", "first");
+                this.attach("attachmentId2", "text/plain", "second");
+                this.attach("attachmentId3", "text/plain", "third");
+                this.attach("attachmentId4", "text/plain", "fourth");
+
+                // list and verify
+                this.listAttachments().count(function(count) {
+                    equal(count, 4, "Attachment size is 4");
+                });
+
+                // get an attachment, play with its properties and download it
+                this.attachment("attachmentId1").then(function() {
+
+                    var length = this.getLength();
+                    ok(length > 0, "Length greater than zero");
+
+                    var attachmentId = this.getId();
+                    equal(attachmentId, "attachmentId1", "Correct attachment ID");
+
+                    var contentType = this.getContentType();
+                    equal("text/plain", contentType, "Correct content type");
+
+                    var uri = this.getUri();
+                    ok(uri, "Computed uri: " + uri);
+
+                    var downloadUri = this.getDownloadUri();
+                    ok(downloadUri, "Computed download uri: " + downloadUri);
+                });
+
+                // walk through all attachments and verify something
+                this.listAttachments().each(function() {
+
+                    var length = this.getLength();
+                    ok(length > 0, "Iteration length > 0");
+                });
+
+                // download attachment
+                this.attachment("attachmentId2").download(function(data) {
+                    ok(data.length > 0, "Download works");
+                });
+
+                // delete attachment (using list + select)
+                this.listAttachments().select("attachmentId3").del();
+
+                // list and verify
+                this.listAttachments().count(function(count) {
+                    equal(count, 3, "Attachment size is 3");
+                });
+
+                // try to request an attachment that doesn't exist and verify
+                this.trap(function() {
+                    ok(true, "Handled missing attachment correctly");
+                }).attachment("missing").getDownloadUri();
+            });
+
+            this.then(function() {
+                success();
+            });
+
+        });
+
+        var success = function()
+        {
+            start();
+        };
+
+    });
+
+}(jQuery) );
