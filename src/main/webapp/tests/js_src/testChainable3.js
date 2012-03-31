ac6456399b6b3758528716f7ed44c63430c8f318
Michael Uzquiano <mgu1@cornell.edu>
2012-03-31 16:07:38 -0400

    Additional test to sanity check subchains on nested each() calls

---
+++ src/main/webapp/tests/js_src/testChainable3.js
@@ -0,0 +1,60 @@
+(function($) {
+
+    module("chainable3");
+
+    // Test case : Chainable 3
+    test("Chainable 3", function()
+    {
+        stop();
+
+        expect(3);
+
+        var gitana = GitanaTest.authenticateFullOAuth();
+        gitana.then(function() {
+
+            // create a repository
+            this.createRepository().readBranch("master").then(function()
+            {
+                var count = 0;
+
+                // create three nodes
+                this.createNode({ "prop": "1" }).then(function() {
+                    this.attach("default", "text/plain", "la la la");
+                });
+                this.createNode({ "prop": "1" }).then(function() {
+                    this.attach("default", "text/plain", "la la la");
+                });
+                this.createNode({ "prop": "1" }).then(function() {
+                    this.attach("default", "text/plain", "la la la");
+                });
+
+                // query for nodes
+                this.queryNodes({
+                    "prop": "1"
+                }).each(function() {
+
+                    this.listAttachments(true).select("default").then(function() {
+
+                        var imageUrl = this.getDownloadUri();
+                        ok(imageUrl, "Found image url");
+
+                        count++;
+
+                        if (count == 3)
+                        {
+                            success();
+                        }
+                    });
+
+                });
+            });
+        });
+
+        var success = function()
+        {
+            start();
+        }
+
+    });
+
+}(jQuery) );
