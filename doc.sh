5db37d366dec5f4181045c39cec0f55ba6c77e27
drq <quyong2000@yahoo.com>
2012-06-17 08:00:07 -0400

    Removed js-doc lib from the project.

--- doc.sh
+++
@@ -1,3 +0,0 @@
-mvn clean install
-
-java -jar jsdoc-toolkit/jsrun.jar jsdoc-toolkit/app/run.js -a -t=jsdoc-toolkit/templates/tably target/gitana-javascript-driver-1.0.0-SNAPSHOT/js/gitana.js -d=doc
