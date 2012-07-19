8f29607f0e34a4b3aca69bf51ec4a81dc9a2e4c5
Michael Uzquiano <mgu1@cornell.edu>
2012-07-19 16:45:12 -0400

    Check for window.location undefined on auto config setup

--- src/main/webapp/js_src/gitana/GitanaAutoConfig.js
+++ src/main/webapp/js_src/gitana/GitanaAutoConfig.js
@@ -3,17 +3,21 @@
     // if we're running on the Cloud CMS hosted platform, we can auto-acquire the client key that we should use
     (function()  {
 
-        var uri = window.location.href;
-        var z1 = uri.indexOf(window.location.pathname);
-        z1 = uri.indexOf("/", z1 + 2);
-        if (z1 > -1)
+        // check to make sure location exists (only available in browsers)
+        if (typeof window.location != "undefined")
         {
-            uri = uri.substring(0, z1);
-        }
+            var uri = window.location.href;
+            var z1 = uri.indexOf(window.location.pathname);
+            z1 = uri.indexOf("/", z1 + 2);
+            if (z1 > -1)
+            {
+                uri = uri.substring(0, z1);
+            }
 
-        if (uri.indexOf("cloudcms.net") > -1)
-        {
-            Gitana.autoConfigUri = uri;
+            if (uri.indexOf("cloudcms.net") > -1)
+            {
+                Gitana.autoConfigUri = uri;
+            }
         }
 
     }());
