70342c59b47ec6aebde89a25d92f9993220b8ad6
Michael Uzquiano <mgu1@cornell.edu>
2012-07-30 22:16:59 -0400

    Added an empty archive test (to be completed later)

---
+++ src/main/webapp/tests/js_src/testArchive1.js
@@ -0,0 +1,28 @@
+(function($) {
+
+    module("archive1");
+
+    // Test case : Archive CRUD operations
+    test("Archive CRUD operations", function()
+    {
+        stop();
+
+        expect(1);
+
+        var gitana = GitanaTest.authenticateFullOAuth();
+        gitana.then(function() {
+
+            var vault = null;
+
+            // create a vault
+            this.createVault().then(function() {
+
+                ok(true, "Successfully created");
+
+                // TODO: how to test archive operations if we can't upload archives...?
+            });
+
+        });
+    });
+
+}(jQuery) );
