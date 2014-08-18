9d08ceece3e0d93cbcc5671ff7ea076d501d914c
uzquiano <mgu1@cornell.edu>
2014-08-18 16:42:42 -0400

    Version bump

--- dist/gitana.js
+++ dist/gitana.js
@@ -1,5 +1,5 @@
 /*
-Gitana JavaScript Driver - Version 1.0.101
+Gitana JavaScript Driver - Version 1.0.102
 
 Copyright 2014 Gitana Software, Inc.
 
@@ -2162,7 +2162,7 @@ if (typeof JSON !== 'object') {
     Gitana.requestCount = 0;
 
     // version of the driver
-    Gitana.VERSION = "1.0.101";
+    Gitana.VERSION = "1.0.102";
 
     // allow for optional global assignment
     // TODO: until we clean up the "window" variable reliance, we have to always set onto window again
