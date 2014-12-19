500498d08192c691d77fb92db6f9c6a457df1565
uzquiano <mgu1@cornell.edu>
2014-12-19 15:40:58 -0500

    add release.sh

---
+++ release.sh
@@ -0,0 +1,70 @@
+VERSION="$(node server/version)"
+BRANCH="$VERSION-release"
+ZIP="gitana-javascript-driver-$VERSION.zip"
+
+echo Deploying version $VERSION
+
+
+#
+# SETUP
+#
+
+# switch to master branch
+# create a local branch <version>-release
+git checkout master
+git checkout -b $BRANCH
+
+
+
+
+#
+# STEP 1: BUILD DRIVER, JSDOCS AND DEPLOY TO CDN
+#
+
+# build driver
+ant clean package
+
+# build jsdoc
+grunt jsdoc
+
+# add the ./dist directory to the commit
+git add dist -f
+
+# commit changes to local branch
+git commit -m "gitana driver release build $VERSION"
+
+
+
+
+#
+# STEP 2: PUBLISH DISTRIBUTION FILES TO CDN
+#
+
+# push to S3
+grunt aws_s3
+
+# invalidate cloudfront (CDN)
+#grunt invalidate_cloudfront
+
+
+
+#
+# STEP 3: TAG REPO FOR BOWER
+#
+
+# create a tag
+git tag $VERSION
+
+# push the tag
+git push origin $VERSION
+
+
+
+
+#
+# TEARDOWN
+#
+
+# delete local branch
+git checkout master
+git branch -D $BRANCH
