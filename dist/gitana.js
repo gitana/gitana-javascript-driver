a3adf43d444f6f2d3391b5a126bf2e4713fd2685
uzquiano <uzi@cloudcms.com>
2020-02-27 14:49:17 -0500

    gitana driver release build 1.0.312

--- dist/gitana.js
+++ dist/gitana.js
@@ -1,5 +1,5 @@
 /*
-Gitana JavaScript Driver - Version ${version}
+Gitana JavaScript Driver - Version 1.0.312
 
 Copyright 2019 Gitana Software, Inc.
 
@@ -2387,7 +2387,7 @@ if (typeof JSON !== 'object') {
     Gitana.requestCount = 0;
 
     // version of the driver
-    Gitana.VERSION = "${version}";
+    Gitana.VERSION = "1.0.312";
 
     // allow for optional global assignment
     // TODO: until we clean up the "window" variable reliance, we have to always set onto window again
